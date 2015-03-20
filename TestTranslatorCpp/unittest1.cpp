b0b78c648c75654464b5d81408597b6fe2f3d54b
oggy <oggy22@gmail.com>
2015-03-19 17:21:01 -0700

    Adding TestTranslatorCpp project

---
+++ TestTranslatorCpp/unittest1.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
