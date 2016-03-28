22582adf5cf3f0355872c1760385138c35989602
oggy <oggy22@gmail.com>
2016-03-27 21:32:34 -0700

    Updating prototype.cpp

--- TranslatorCpp/prototype.cpp
+++ TranslatorCpp/prototype.cpp
@@ -3,16 +3,22 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <map>
 
 using namespace std;
 
-enum class word_type { noun, verb, pron };
+enum class word_type { noun, verb, pron, NounPhrase, VerbPhrase, Preposition, Sentence };
 enum class categories { gender, person, plurality };
 enum class attributes { person1, person2, person3, male, female, neutral, singular, plural };
 
 #define noun word_type::noun
 #define verb word_type::verb
 #define pron word_type::pron
+#define NP word_type::NounPhrase
+#define VP word_type::VerbPhrase
+#define Prep word_type::Preposition
+#define Sent word_type::Sentence
+
 #define gen categories::gender
 #define per categories::person
 #define plrt categories::plurality
@@ -22,40 +28,79 @@ enum class attributes { person1, person2, person3, male, female, neutral, singul
 #define mal attributes::male
 #define fem attributes::female
 #define neut attributes::neutral
-#define sign attributes::singular
+#define sing attributes::singular
 #define plur attributes::plural
+
+const std::map<attributes, categories> belongs
+{
+	{ per1, per },
+	{ per2, per },
+	{ per3, per },
+	{ mal, gen },
+	{ fem, gen },
+	{ neut, gen },
+	{ sing, plrt },
+	{ plur, plrt },
+};
 
 class node
 {
-	set<attributes> attrs;
-	set<categories> cats;
+	string st;
+	set<attributes> free_attrs;
+	set<categories> free_cats;
+	map<categories, attributes> mapping;
 
 public:
+	node(const string& st) : st(st)
+	{
+	}
+
 	node(word_type wordtype)
 	{
 		cout << "node(word_type)" << endl;
 	}
 
 	template <typename... Values>
-	node(word_type wordtype, attributes a, Values... values) : node(wordtype, values...)
+	node(word_type wordtype, attributes attr, Values... values) : node(wordtype, values...)
 	{
 		cout << "node(word_type, attr, ...)" << endl;
-		attrs.insert(a);
+		auto p_cat = belongs.find(attr);
+		if (p_cat != belongs.end())
+		{
+			auto cat = p_cat->second;
+			if (mapping.count(cat) > 0)
+				throw "The attribute from the same category set already";
+			mapping[cat] = attr;
+		}
+		else
+		{
+			if (free_attrs.count(attr) > 0)
+				throw "The attribute already added";
+			free_attrs.insert(attr);
+		}
 	}
 
 	template <typename... Values>
-	node(word_type wordtype, categories c, Values... values) : node(wordtype, values...)
+	node(word_type wordtype, categories cat, Values... values) : node(wordtype, values...)
 	{
 		cout << "node(word_type, cat, ...)" << endl;
-		cats.insert(c);
+		auto p_cat = mapping.find(cat);
+		if (p_cat != mapping.end())
+			throw "The category alreay mapped";
+
+		free_cats.insert(cat);
 	}
 };
 
 class rule
 {
 public:
-	using right_type = vector<node>;
+	using right_type = std::vector<node>;
 	using size_type = typename right_type::size_type;
+	
+	node left;
+	vector<node> right;
+
 	set<attributes> attrs;
 	set<categories> cats;
 
@@ -63,9 +108,6 @@ public:
 	{
 		attrs.clear();
 	}
-
-	node left;
-	vector<node> right;
 
 	size_type size() const
 	{
@@ -78,4 +120,9 @@ int main()
 	node n1(noun, categories::gender);
 	node n2(noun, attributes::person1, attributes::female, categories::plurality);
 	node n3{pron, attributes::singular };
+
+	rule r1{ {Prep}, { {"in"}, {noun}} };
+	rule r2{ {Sent}, {{pron, per3}, {verb, per3, sing}} };
+
+	//cout << attributes::female << endl;
 }
