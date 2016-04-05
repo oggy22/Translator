f59c2c79129437aa2e66d5a9bf0e36613e908cb5
Ognjen Sobajic <oggy22@gmail.com>
2016-04-04 23:55:03 -0700

    Updating Serbian, adding some tests, adding Serbian alphabet

--- TestTranslatorCpp/serbian_test.cpp
+++ TestTranslatorCpp/serbian_test.cpp
@@ -18,6 +18,18 @@ namespace TranslatorTest
 		Serbian serbian;
 
 	public:
+		TEST_METHOD(every_word_composed_of_serbian_letters)
+		{
+			for (const auto& word : serbian.dictWords)
+			{
+				for (const auto& w : word.words)
+				{
+					for (wchar_t c : w._word)
+						Assert::AreNotEqual(std::wstring::npos, serbian.stAlphabet.find(c));
+				}
+			}
+		}
+
 		TEST_METHOD(every_noun_has_gender)
 		{
 			for (const auto& word : serbian.dictWords)
@@ -29,6 +41,24 @@ namespace TranslatorTest
 						word.attrs.count(Serbian::attributes::женски) ||
 						word.attrs.count(Serbian::attributes::средњи));
 				}
+			}
+		}
+
+		TEST_METHOD(each_noun_has_7cases)
+		{
+			for (const auto& word : serbian.dictWords)
+			{
+				if (word.wordtype == Serbian::word_type::именица)
+					Assert::AreEqual<int>(7 * 2, word.words.size(), (word.word + L" doesn't have 14 forms").c_str());
+			}
+		}
+
+		TEST_METHOD(each_verb_has_3x2_present_forms)
+		{
+			for (const auto& word : serbian.dictWords)
+			{
+				if (word.wordtype == Serbian::word_type::глагол)
+					Assert::AreEqual<int>(3 * 2, word.words.size(), (word.word + L" doesn't have 6 forms").c_str());
 			}
 		}
 
