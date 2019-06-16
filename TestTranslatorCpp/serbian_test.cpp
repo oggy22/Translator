b0df40caf93a48b5cc22ae75579279a04ddf2474
Ognjen Sobajic <oggy22@gmail.com>
2019-06-15 17:58:44 -0700

    Adding Serbian adjecitive "go"

--- TestTranslatorCpp/serbian_test.cpp
+++ TestTranslatorCpp/serbian_test.cpp
@@ -355,7 +355,7 @@ namespace TranslatorTest
 		TEST_METHOD(dictionary_words_count)
 		{
 			// Update this number when necessary
-			Assert::AreEqual<int>(597, Serbian::dictWords().size());
+			Assert::AreEqual<int>(599, Serbian::dictWords().size());
 		}
 
 		// This test helps keeping awereness of the number of word forms.
@@ -371,7 +371,7 @@ namespace TranslatorTest
 			}
 
 			// Update this number when necessary
-			Assert::AreEqual<int>(13498, count);
+			Assert::AreEqual<int>(13549, count);
 		}
 
 		TEST_METHOD(serbian_numbers_to_1000)
