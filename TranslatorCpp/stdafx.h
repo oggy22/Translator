f7d809f1f109781ad6700c52ec56d6b439b8f4ea
Ognjen Sobajic <oggy22@gmail.com>
2020-04-11 21:24:38 -0700

    Using always std::set and std::map (instead of std::unordered_set and std::unordered_map) which seem to overperf in Release

--- TranslatorCpp/stdafx.h
+++ TranslatorCpp/stdafx.h
@@ -7,17 +7,10 @@
 #include <iostream>
 #include <experimental/generator>
 
-#ifdef _DEBUG
 #include <set>
 #include <map>
 #define myset std::set
 #define map std::map
-#else
-#include <unordered_set>
-#include <unordered_map>
-#define myset std::unordered_set
-#define map std::unordered_map
-#endif
 
 #include "parsing_triangle.h"
 #include "Assert.h"
