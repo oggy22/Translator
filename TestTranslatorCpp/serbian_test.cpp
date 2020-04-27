f2cfeb29af106090d989924d312406ec64db4f1b
Ognjen Sobajic <oggy22@gmail.com>
2020-04-26 17:26:55 -0700

    Adding more Serbian words.

--- TestTranslatorCpp/serbian_test.cpp
+++ TestTranslatorCpp/serbian_test.cpp
@@ -431,7 +431,7 @@ namespace TranslatorTest
 		TEST_METHOD(dictionary_words_count)
 		{
 			// Update this number when necessary
-			Assert::AreEqual<int>(1087, Serbian::dictWords().size());
+			Assert::AreEqual<int>(1108, Serbian::dictWords().size());
 		}
 
 		// This test helps keeping awereness of the number of word forms.
@@ -447,7 +447,7 @@ namespace TranslatorTest
 			}
 
 			// Update this number when necessary
-			Assert::AreEqual<int>(24912, count);
+			Assert::AreEqual<int>(25400, count);
 		}
 
 		TEST_METHOD(serbian_numbers_to_1000)
